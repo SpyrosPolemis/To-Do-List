92a9b8000a9fa757970b903083c140c0cfb227a2
SpyrosPolemis <spy.polemis@gmail.com>
2026-04-17 02:07:56 +1000

    Create uiController with updateLists method

---
+++ src/uiController.js
@@ -0,0 +1,25 @@
+import listController from "./listController.js";
+import listIcon from "./assets/menu.svg"
+
+const sidebarContent = document.querySelector("#sidebar-content");
+const centreWindow = document.querySelector("#centre-window")
+
+const uiController = {
+    updateLists() {
+        sidebarContent.innerHTML = ""
+        const lists =  listController.getListNames()
+        for (let i = 0; i < lists.length; i++) {
+            const listDiv = document.createElement("div");
+            const icon = document.createElement("img");
+            icon.src = listIcon;
+            icon.draggable = false;
+            listDiv.append(icon, lists[i]);
+            listDiv.classList.add("list-entry", "no-select");
+            sidebarContent.append(listDiv);
+        }
+    }
+}
+
+export default uiController
+
+
